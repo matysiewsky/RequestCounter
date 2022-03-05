941f2ccecd6b3eabb4d9b5195b8ce73f40b6e01b
Maciej M <matysiewskimaciej@gmail.com>
2022-03-05 09:35:41 +0100

    Initial commit

---
+++ RequestCounter.Services/DataReader.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace RequestCounter.Services
+{
+    /// <summary>
+    /// This class is supposed to be implemented as part of TASK "Save counts". Pay attention to setting of the file added to project
+    /// </summary>
+    internal static class DataReader
+    {
+        private static readonly string fileName = "request-counts.txt";
+
+        internal static Dictionary<string, int> ReadFromFile()
+        {
+            throw new NotImplementedException();
+           //Put code reading data from file here
+        }
+
+        internal static void WriteToFile(Dictionary<string, int> stats)
+        {
+            throw new NotImplementedException();
+            //Put code writing to file here
+        }
+
+        /// <summary>
+        /// Use it to resolve full path to the file. 
+        /// </summary>
+        /// <returns></returns>
+        private static string GetFullFilePath()
+        {
+            string filePath = Path.GetDirectoryName(System.AppDomain.CurrentDomain.BaseDirectory);
+            var fullPath = Path.Combine(filePath, fileName);
+            return fullPath;
+        }
+    }
+}
---
+++ RequestCounter.Services/RequestCountStatisticsService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RequestCounter.Services
+{
+    /// <summary>
+    /// Use this class to implement IRequestCountStatsService
+    /// </summary>
+    public class RequestCountStatisticsService
+    {     
+        //List of methods which are allowed to be counted by designed service
+        private static readonly string[] AllowedMethods = new string[] { "GET", "POST", "DELETE", "PATCH", "PUT" };
+    }
+}
